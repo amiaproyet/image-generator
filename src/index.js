bccc6dd1bef2ec203d65d043b6f16252dabc5f02
amiaproyet <121839948+amiaproyet@users.noreply.github.com>
2023-02-10 12:16:52 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import App from "./App";
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+import searchImages from "./api";
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+    <App />
+)
